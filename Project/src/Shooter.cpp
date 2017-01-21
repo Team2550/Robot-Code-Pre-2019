cb583cb1bb0ef2d85fbe9c5b3d5042ff8f0a31b6
Lance C. Booth <lancecomputerwizz@gmail.com>
2017-01-20 22:09:09 -0800

    Implemented barebones shooter code
    Basically, you call shoot() and it puts the motor at full power; call stop(),
    and the motor stops.

--- Project/src/Shooter.cpp
+++ Project/src/Shooter.cpp
@@ -3,10 +3,28 @@
 Shooter::Shooter(int motorPort) :
                 motor(motorPort)
 {
-
+	isShooting = false;
 }
 
 Shooter::~Shooter()
 {
 
+}
+
+void Shooter::shoot(float power)
+{
+	if(!isShooting)
+	{
+		motor.Set(power);
+		isShooting = true;
+	}
+}
+
+void Shooter::stop()
+{
+	if(isShooting)
+	{
+		motor.Set(0);
+		isShooting = false;
+	}
 }
--- Project/src/Shooter.h
+++ Project/src/Shooter.h
@@ -8,8 +8,12 @@ class Shooter
 public:
 	Shooter(int motorPort);
 	~Shooter();
+
+	void shoot(float power = 1);
+	void stop();
 private:
 	Spark motor; // If anyone has an idea for a better name, go ahead and change it!
+	bool isShooting;
 };
 
 #endif
