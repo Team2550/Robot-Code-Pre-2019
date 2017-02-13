3c418c7eb9c7e2ae5de3f4f02da07c550dd21937
Nathan Walker <nathanwalker1208@gmail.com>
2017-02-12 17:31:07 -0800

    Commited changes
    Much of the code is reworked to work on my home setup. Currently working on getting rectangles to be detected. Not yet finished

--- OpenCV/Nathan - tracking.py
+++ OpenCV/Nathan - tracking.py
@@ -15,6 +15,37 @@ UDP_PORT = 8890
 
 print("Found roboRIO at", UDP_IP)
 
+def detectShapes(image):
+    blurred = cv2.GaussianBlur(image, (5, 5), 0)
+    thresh = cv2.threshold(blurred, 60, 255, cv2.THRESH_BINARY)[1]
+    RGB_image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
+    
+    cv2.imshow("Image", thresh)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()    
+
+    # find contours in the thresholded image
+    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+	cv2.CHAIN_APPROX_SIMPLE)[1]
+
+    for c in cnts:
+        # detect shape
+        peri = cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, 0.04 * peri, True)
+
+        if len(approx) == 4:
+            (x, y, w, h) = cv2.boundingRect(approx)
+            
+            print("Found rectangle, size", w * h, ":", x, y, w, h)
+            #ar = w / float(h)
+            
+            #if ar >= 0.95 and ar <= 1.05:
+            cv2.drawContours(RGB_image, [c], 0, (0, 255, 0), 4)
+ 
+    # show the output image
+    cv2.imshow("Image", RGB_image)
+    #cv2.waitKey(0)    
+
 def processCamera(capWebcam):
     blnFrameReadSuccessfully, imgOriginal = capWebcam.read()            # read next frame
 
@@ -23,47 +54,43 @@ def processCamera(capWebcam):
 
         return -1, -1, -1
 
-    try:
-        imgHSV = cv2.cvtColor(imgOriginal, cv2.COLOR_BGR2HSV)
+    #try:
+    imgHSV = cv2.cvtColor(imgOriginal, cv2.COLOR_BGR2HSV)
+    
+    lowerBound = np.array([0, 0, 200])
+    upperBound = np.array([255, 50, 255])
+
+    mask = cv2.inRange(imgHSV, lowerBound, upperBound)
+
+    #res = cv2.bitwise_and(imgOriginal, imgOriginal, mask = mask)
+    
+    #cv2.imshow("Original Image", imgOriginal);
+    #cv2.imshow("Masked Image", mask);
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()
+    #cv2.imwrite("maskedImage.png", res);
+
+    detectShapes(mask)
+    
+    #imgThresh = cv2.add(imgThreshLow, imgThreshHigh)
+    #imgThresh = cv2.GaussianBlur(imgThresh, (3, 3), 2)
+    #imgThresh = cv2.dilate(imgThresh, np.ones((5,5), np.uint8))
+    #imgThresh = cv2.erode(imgThresh, np.ones((5,5), np.uint8))
+
+    #intRows, intColumns = imgThresh.shape
+
+    ##circles = cv2.HoughCircles(imgThresh, cv2.HOUGH_GRADIENT, 5, intRows / 4)      # fill variable circles with all circles in the processed image
+
+    #if circles is not None: # this line is necessary to keep program from crashing on next line if no circles were found
+    #    for circle in circles[0]:
+    #        return circle
         
-        lowerGreen = np.array([90, 50, 75])
-        upperGreen = np.array([130, 255, 255])
-
-        mask = cv2.inRange(imgHSV, lowerGreen, upperGreen)
-
-        #res = cv2.bitwise_and(imgOriginal, imgOriginal, mask = mask)
+    return -1, -1, -1
         
-        cv2.imwrite("originalImage.png", imgOriginal);
-        cv2.imwrite("maskImage.png", mask);
-        #cv2.imwrite("maskedImage.png", res);
-
-        circles = cv2.HoughCircles(mask,cv2.HOUGH_GRADIENT,1,20,
-                            param1=50,param2=30,minRadius=10,maxRadius=0)
-
-        if circles is not None:
-            print("Found circles: ")
-            for i in circles:
-                print(i, ", ")
+    #except Exception:
+    #    print("Error in image processing!")
         
-        #imgThresh = cv2.add(imgThreshLow, imgThreshHigh)
-        #imgThresh = cv2.GaussianBlur(imgThresh, (3, 3), 2)
-        #imgThresh = cv2.dilate(imgThresh, np.ones((5,5), np.uint8))
-        #imgThresh = cv2.erode(imgThresh, np.ones((5,5), np.uint8))
-
-        #intRows, intColumns = imgThresh.shape
-
-        ##circles = cv2.HoughCircles(imgThresh, cv2.HOUGH_GRADIENT, 5, intRows / 4)      # fill variable circles with all circles in the processed image
-
-        #if circles is not None: # this line is necessary to keep program from crashing on next line if no circles were found
-        #    for circle in circles[0]:
-        #        return circle
-            
-        return -1, -1, -1
-        
-    except Exception:
-        print("Error in image processing!")
-        
-        return -1, -1, -1
+    #    return -1, -1, -1
 
 def main():
     #capWebcam = cv2.VideoCapture(CAMERA_URL) # declare a VideoCapture object and associate to webcam, 0 => use 1st webcam
@@ -124,3 +151,4 @@ def main():
 if __name__ == "__main__":
 
     main()
+    cv2.destroyAllWindows()
