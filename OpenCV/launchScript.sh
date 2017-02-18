581cb360b83e343a749b805e187a24dd9b1391a3
Nathan Walker <nathanwalker1208@gmail.com>
2017-02-18 14:03:19 -0800

    Optimizations
    ---Deactivated auto-shutdown
    +++Decreased streaming resolution
    >>>Fixed FOV
    +++Added ability to run on PC or Raspi

--- OpenCV/launchScript.sh
+++ OpenCV/launchScript.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-/home/pi/mjpg-streamer/mjpg-streamer/mjpg_streamer -i "/home/pi/mjpg-streamer/mjpg-streamer/input_uvc.so -y -f 30" -o "/home/pi/mjpg-streamer/mjpg-streamer/output_http.so -w /home/pi/mjpg-streamer/mjpg-streamer/www" -b
+/home/pi/mjpg-streamer/mjpg-streamer/mjpg_streamer -i "/home/pi/mjpg-streamer/mjpg-streamer/input_uvc.so -y -f 20 -r 320x240" -o "/home/pi/mjpg-streamer/mjpg-streamer/output_http.so -w /home/pi/mjpg-streamer/mjpg-streamer/www" -b
 source /usr/local/bin/virtualenvwrapper.sh
 workon cv
 python ~/targetTracking.py
 deactivate
 killall mjpg_streamer
-sudo shutdown -h now
+#sudo shutdown -h now
