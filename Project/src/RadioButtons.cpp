9af7a853a30dea4ddf7e9310f8234b9022364e57
Lance C. Booth <lancecomputerwizz@gmail.com>
2017-03-22 09:46:22 -0700

    Implemented functions for RadioButtons (untested)

--- Project/src/RadioButtons.cpp
+++ Project/src/RadioButtons.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 
 template<class E>
-RadioButtons<E>::RadioButtons(std::string name)
+RadioButtons<E>::RadioButtons(std::string name) : sendableChooser(name)
 {
 
 }
@@ -16,19 +16,42 @@ RadioButtons<E>::~RadioButtons()
 }
 
 template<class E>
-void RadioButtons<E>::AddObject(std::string, E enumValue)
+void RadioButtons<E>::AddObject(std::string name, E enumValue)
 {
-
+	try
+	{
+		enumPointers.at(name);
+	}
+	catch(const std::out_of_range &e)
+	{
+		enumPointers.insert(name, new E(enumValue));
+		sendableChooser.AddObject(name, enumPointers.at(name));
+	}
 }
 
 template<class E>
-void RadioButtons<E>::AddDefault(std::string, E enumValue)
+void RadioButtons<E>::AddDefault(std::string name, E enumValue)
 {
-
+	try
+	{
+		enumPointers.at(name);
+	}
+	catch(const std::out_of_range &e)
+	{
+		enumPointers.insert(name, new E(enumValue));
+		sendableChooser.AddDefault(name, enumPointers.at(name));
+	}
 }
 
 template<class E>
 E RadioButtons<E>::GetSelected()
 {
-
+	E *selection = sendableChooser.GetSelected();
+	if(selection == nullptr)
+	{
+		std::cerr << "Radio button not received; undefined behavior starting now..." << std::endl;
+		return E(0);
+	}
+	else
+		return *selection;
 }
--- Project/src/RadioButtons.h
+++ Project/src/RadioButtons.h
@@ -3,6 +3,7 @@
 
 #include <string>
 #include <map>
+#include <WPILib.h>
 
 template<class E>
 class RadioButtons
@@ -11,11 +12,12 @@ public:
 	RadioButtons(std::string name);
 	~RadioButtons();
 
-	void AddObject(std::string, E enumValue);
-	void AddDefault(std::string, E enumValue);
+	void AddObject(std::string name, E enumValue);
+	void AddDefault(std::string name, E enumValue);
 	E GetSelected();
 private:
 	std::map<std::string, E*> enumPointers;
+	SendableChooser<E*> sendableChooser;
 };
 
 #endif
