80726032e5a47e815dd02bbfd3a1f0a2605d42d2
Nathan Walker <nathanwalker1208@gmail.com>
2017-03-26 16:08:34 -0700

    Decreased exposure

--- OpenCV/Nathan - launchScript.sh
+++ OpenCV/Nathan - launchScript.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mjpg_streamer -i "input_uvc.so -y -f 20 -r 640x480" -o "output_http.so -w /home/pi/mjpg-streamer/mjpg-streamer/www" -b
 v4l2-ctl -c exposure_auto=1
-v4l2-ctl -c exposure_absolute=10
+v4l2-ctl -c exposure_absolute=0
 source /usr/local/bin/virtualenvwrapper.sh
 workon cv
 
