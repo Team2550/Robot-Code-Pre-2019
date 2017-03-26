5ce6f902671fecb976ba461b6964f6114d153414
Nathan Walker <nathanwalker1208@gmail.com>
2017-03-26 09:30:02 -0700

    Added absolute exposure, updated my files
    Exposure is now set very low, which reduces motion blur and increases the amount by which the target will stand out. Filters need to be corrected. Mjpg_streamer should be installed completely on the pi.

--- OpenCV/Nathan - tracking.py
+++ OpenCV/Nathan - tracking.py
@@ -3,21 +3,30 @@ import numpy as np
 import os
 import socket
 import math
+import time
 
 #################################################################################################################
 
-TARGET_RECT_SIZE_INCHES = (2, 3.5)
+MAX_FRAMERATE = 20
+
+OPEN_WINDOWS = False
+
+TARGET_RECT_SIZE_INCHES = (2, 5)
 TARGET_RECT_DIAGONAL_INCHES = (TARGET_RECT_SIZE_INCHES[0]**2 + TARGET_RECT_SIZE_INCHES[1]**2)**(0.5)
+TARGET_YOFFSET = 2.5
+TARGET_MAX_YOFFSET = 20
 
 TARGET_ASPECT_RATIO = min(TARGET_RECT_SIZE_INCHES) / max(TARGET_RECT_SIZE_INCHES) # Always less than one
-TARGET_ASPECT_MARGIN_OF_ERROR = 0.15 # Percentage that any rectangles seen can defer from the known aspect ratio
+TARGET_ASPECT_MARGIN_OF_ERROR = 0.40 # Percentage that any rectangles seen can defer from the known aspect ratio
 
-ACCEPTABLE_SIDE_PERCENT_DIFFERENCE = 0.3
+SHAPE_BOUNDINGBOX_ASPECT_RATIO_MAX_DIFF = 0.40 # Maximum difference between a shapes aspect ratio and its bounding box's aspect ratio (percentage)
 
-IMAGE_SIZE = (IMAGE_WIDTH, IMAGE_HEIGHT) = (320, 240)
+ACCEPTABLE_SIDE_PERCENT_DIFFERENCE = 0.5
+
+IMAGE_SIZE = (IMAGE_WIDTH, IMAGE_HEIGHT) = (640, 480)
 IMAGE_DIAGONAL = (IMAGE_SIZE[0]**2 + IMAGE_SIZE[1]**2)**(0.5)
 
-CAMERA_FOV = 66 # FOV of axis camera, needs to change for USB camera 
+CAMERA_FOV = 68.5 # FOV of axis camera, needs to change for USB camera 
 
 #################################################################################################################
 
@@ -25,58 +34,56 @@ COTANGENT_FOV = 1 / math.tan(math.radians(CAMERA_FOV / 2.0))
 
 CAMERA_URL = 0
 
-print("Searching for camera...")
+print("Searching for local camera...")
 try:
-    CAMERA_IP = socket.gethostbyname("axis-camera.local")
-    CAMERA_URL = "http://" + CAMERA_IP + "/mjpg/video.mjpg"
+    CAMERA_IP = socket.gethostbyname("raspberrypi")
+    CAMERA_URL = "http://" + CAMERA_IP + ":8080/?action=stream&amp;type=.mjpg"
     print("Found camera at", CAMERA_IP)
 except Exception:
-    CAMERA_URL = 0
-    print("No IP camera found. Defaulting to USB camera.")
+    try:
+        print("Searching for network camera...")
+        CAMERA_IP = socket.gethostbyname("raspberrypi.local")
+        CAMERA_URL = "http://" + CAMERA_IP + ":8080/?action=stream&amp;type=.mjpg"
+        print("Found camera at", CAMERA_IP)
+    except Exception:
+        CAMERA_URL = 0
+        print("No IP camera found. Defaulting to USB camera.")
 
-UDP_IP = "10.25.50.20"
+UDP_IP = "192.168.2.201"
 
-print("Searching for roboRIO...")
+print("Searching for PC...")
 try:
     UDP_IP = socket.gethostbyname("NW-GAMING")    #declares udp ip and port
-    print("Found roboRIO at", UDP_IP)
+    print("Found PC at", UDP_IP)
 except Exception:
-    UDP_IP = "127.0.0.1"
-    print("RoboRIO not found. Defaulting to static IP.")
+    UDP_IP = "192.168.2.201"
+    print("PC not found. Defaulting to static IP.")
     
 UDP_PORT = 8890
 
 def dist(x1, y1, x2, y2):
     return ((x1 - x2)*(x1 - x2)+(y1 - y2)*(y1 - y2))**(0.5)
 
-def processCamera(camCapture):
-    blnFrameReadSuccessfully, imgOriginal = camCapture.read()            # read next frame
-
-    if (not blnFrameReadSuccessfully) or (imgOriginal is None):             # if frame was not read successfully
-        print("Error: frame not read from webcam\n")                     # print error message to std out
-
-        return None
-
+def processCamera(imgOriginal):
     imgHSV = cv2.cvtColor(imgOriginal, cv2.COLOR_BGR2HSV)
     
-    lowerBound = np.array([0, 0, 245])
-    upperBound = np.array([255, 30, 255])
+    lowerBound = np.array([0, 0, 230])
+    upperBound = np.array([255, 100, 255])
 
     mask = cv2.inRange(imgHSV, lowerBound, upperBound)
-
-    outline = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
-
+    maskDraw = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
+    
     # find contours in the thresholded image
     cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
                             cv2.CHAIN_APPROX_SIMPLE)[1]
 
+    dataPoints = []
+    
     for c in cnts:
         peri = cv2.arcLength(c, True)
         approx = cv2.approxPolyDP(c, 0.04 * peri, True)
 
         if len(approx) == 4:
-            cv2.drawContours(outline, [approx], -1, (255,0,0), 1)
-            
             sideLengths = [0, 0, 0, 0]
             for i in range(4):
                 sideLengths[i] = dist(approx[i][0][0], approx[i][0][1],
@@ -90,6 +97,17 @@ def processCamera(camCapture):
 
             shortSide = min(width, height)
             longSide = max(width, height)
+
+            # Find corners of bounding box
+            maxX = max(approx[0][0][0],approx[1][0][0],approx[2][0][0],approx[3][0][0])
+            maxY = max(approx[0][0][1],approx[1][0][1],approx[2][0][1],approx[3][0][1])
+            minX = min(approx[0][0][0],approx[1][0][0],approx[2][0][0],approx[3][0][0])
+            minY = min(approx[0][0][1],approx[1][0][1],approx[2][0][1],approx[3][0][1])
+
+            # Calculate width, height, and aspect ratio of bounding box. 
+            boundboxWidth = maxX - minX
+            boundboxHeight = maxY - minY
+            boundboxAR = boundboxWidth / boundboxHeight
             
             size = width * height
             ar = shortSide / longSide # Aspect ratio: will always be less than one
@@ -97,7 +115,8 @@ def processCamera(camCapture):
             if (size > 250 and ar >= TARGET_ASPECT_RATIO * (1 - TARGET_ASPECT_MARGIN_OF_ERROR) and
                                ar <= TARGET_ASPECT_RATIO * (1 + TARGET_ASPECT_MARGIN_OF_ERROR) and
                                widthSideDiff < ACCEPTABLE_SIDE_PERCENT_DIFFERENCE and
-                               heightSideDiff < ACCEPTABLE_SIDE_PERCENT_DIFFERENCE):
+                               heightSideDiff < ACCEPTABLE_SIDE_PERCENT_DIFFERENCE and
+                               abs(boundboxAR / ar - 1) < SHAPE_BOUNDINGBOX_ASPECT_RATIO_MAX_DIFF):
                                   
                 diagonal = (dist(approx[0][0][0],approx[0][0][1],
                                  approx[2][0][0],approx[2][0][1]) +
@@ -107,10 +126,6 @@ def processCamera(camCapture):
                 avgCoordinates = (avgX, avgY) = ((approx[0][0][0]+approx[1][0][0]+approx[2][0][0]+approx[3][0][0])/4,
                                                  (approx[0][0][1]+approx[1][0][1]+approx[2][0][1]+approx[3][0][1])/4)
 
-                cv2.drawContours(outline, [c], -1, (0,255,0), 2)
-                cv2.drawContours(outline, [approx], -1, (255,0,0), 1)
-                cv2.circle(outline, (int(avgX), int(avgY)), 4, (0,0,255), -1)
-                
                 percentOfView = diagonal / IMAGE_DIAGONAL
                 diagonalOfObjectPlaneInches = TARGET_RECT_DIAGONAL_INCHES / percentOfView
                 widthOfObjectPlaneInches = (IMAGE_WIDTH / IMAGE_DIAGONAL) * diagonalOfObjectPlaneInches
@@ -118,20 +133,40 @@ def processCamera(camCapture):
                 
                 objectDist = diagonalOfObjectPlaneInches * 0.5 * COTANGENT_FOV
                 objectXOffset = ((avgX / IMAGE_WIDTH) - (1/2)) * widthOfObjectPlaneInches
-                objectYOffset = ((avgY / IMAGE_HEIGHT) - (1/2)) * heightOfObjectPlaneInches
-                
-                xAngle = math.degrees(math.atan2(objectXOffset, objectDist))
-                yAngle = math.degrees(math.atan2(objectYOffset, objectDist))
-                
-                #print("Found rectangle,", size, ":", ar, ":", x, y, width, height, "Dist:", objectDist)
+                objectYOffset = -((avgY / IMAGE_HEIGHT) - (1/2)) * heightOfObjectPlaneInches
 
-                cv2.imshow('Mask',outline)
+                if (abs(objectYOffset - TARGET_YOFFSET) < TARGET_MAX_YOFFSET):
+                    xAngle = math.degrees(math.atan2(objectXOffset, objectDist))
+                    yAngle = math.degrees(math.atan2(objectYOffset, objectDist))
 
-                return objectDist, objectXOffset, objectYOffset, xAngle, yAngle # Test values
+                    percentMatch = 1 - abs(ar / TARGET_ASPECT_RATIO - 1)
+                    percentMatch *= 1 - abs(boundboxAR / ar - 1)
+                    percentMatch *= 1 - ((objectYOffset - TARGET_YOFFSET) / (TARGET_MAX_YOFFSET))**2
 
-    cv2.imshow('Mask',outline)
+                    if (OPEN_WINDOWS):
+                        cv2.drawContours(imgOriginal, [approx], -1, (0,255,0), 2)
+                        cv2.drawContours(maskDraw, [approx], -1, (0,255,0), 2)
+                        cv2.putText(imgOriginal, "{0:.1f}".format(percentMatch * 100) + "% match, " + "{0:.2f}".format(objectDist) + " inches, " + 
+                                    "{0:.2f}".format(xAngle) + " degrees",
+                                    (minX, minY), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (255,0,0))
+                    
+                    dataPoints += [(percentMatch, objectDist, objectXOffset, objectYOffset, xAngle, yAngle)] # Test values
+
+            else:
+                if (OPEN_WINDOWS):
+                    cv2.drawContours(imgOriginal, [approx], -1, (255,0,0), 1)
+                    cv2.drawContours(maskDraw, [approx], -1, (255,0,0), 1)
+                    cv2.putText(imgOriginal, "{0:.1f}".format(abs(ar / TARGET_ASPECT_RATIO - 1) * 100) + "% AR mismatch",
+                                (maxX, minY), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0,0,255))
+
+    if (OPEN_WINDOWS):
+        try:
+            cv2.imshow('Original', imgOriginal)
+            cv2.imshow('Mask', maskDraw)
+        except Exception:
+            print("Can't display window!")
     
-    return None
+    return dataPoints
 
 def main():
     camCapture = cv2.VideoCapture(CAMERA_URL) # declare a VideoCapture object and associate to webcam, 0 => use 1st webcam
@@ -151,29 +186,49 @@ def main():
         
         return                                                          # and exit function (which exits program)
         
-    test_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sendingSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    
+    receivingSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    receivingSocket.bind(("localhost", UDP_PORT))
 
     running = True
 
     try:
+        print("Beginning image processing...")
         while (running and cv2.waitKey(1) != 27 and camCapture.isOpened()):
-            try:            
-                data = processCamera(camCapture)
+            startTime = time.time()
+            try:
+                blnFrameReadSuccessfully, imgOriginal = camCapture.read() # read next frame    
 
-                if (data is not None):
-                    dist, xOffset, yOffset, horizAngle, vertAngle = data
-                    
-                    print("Found target (dist, xOffset, yOffset, horizAngle, vertAngle):", dist, xOffset, yOffset, horizAngle, vertAngle)
-                    
-                    arr = [dist, xOffset, yOffset, horizAngle, vertAngle]
-                    data = ' '.join(str(x) for x in arr)
-                    test_socket.sendto(bytes(data, 'utf-8'), (UDP_IP, UDP_PORT)) #sends array to socket
-                    print("Sent data to RoboRIO!")
+                if (not blnFrameReadSuccessfully) or (imgOriginal is None):             # if frame was not read successfully
+                    print("Error: frame not read from webcam\n")                     # print error message to std out
+                else:
+                    data = processCamera(imgOriginal)
+
+                    if (data is not None and len(data) > 1):
+                        #print("Found", len(data), "targets")
+
+                        for d in data:
+                            percentMatch, dist, xOffset, yOffset, horizAngle, vertAngle = d
+                            #print("Found target (percentMatch, dist, xOffset, yOffset, horizAngle, vertAngle):", percentMatch, dist, xOffset, yOffset, horizAngle, vertAngle)
+                            #print("Found target: ", horizAngle)
+                        
+                        data = ','.join(' '.join("{0:.3f}".format(y) for y in x) for x in data)
+
+                        sendingSocket.sendto(bytes(data, 'utf-8'), (UDP_IP, UDP_PORT)) #sends array to socket
+                        #print("Sent data to RoboRIO!")
+                    #else:
+                        #print("No target found.")
+
+                # Keep buffer empty until next frame
+                while ((time.time() - startTime) < (1./MAX_FRAMERATE)):
+                    blnFrameReadSuccessfully, imgOriginal = camCapture.read()
 
             except Exception:
                 running = False
 
-                test_socket.close()
+                sendingSocket.close()
+                receivingSocket.close()
                 camCapture.release()
                 
                 raise
@@ -181,7 +236,8 @@ def main():
     except KeyboardInterrupt: # Catch exit by Ctrl-C
         print("\nTerminating script...")
         
-        test_socket.close()
+        sendingSocket.close()
+        receivingSocket.close()
         camCapture.release()
     
     return
--- OpenCV/launchScript.sh
+++ OpenCV/launchScript.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 /home/pi/mjpg-streamer/mjpg-streamer/mjpg_streamer -i "/home/pi/mjpg-streamer/mjpg-streamer/input_uvc.so -y -f 20 -r 640x480" -o "/home/pi/mjpg-streamer/mjpg-streamer/output_http.so -w /home/pi/mjpg-streamer/mjpg-streamer/www" -b
+v4l2-ctl -c exposure_auto=1
+v4l2-ctl -c exposure_absolute=10
 source /usr/local/bin/virtualenvwrapper.sh
 workon cv
 
