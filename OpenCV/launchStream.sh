81a7c55b870d988d3d90280588ec6a89d0013ebc
Nathan Walker <nathanwalker1208@gmail.com>
2017-03-26 17:55:57 -0700

    Added launch stream file

--- OpenCV/launchStream.sh
+++ OpenCV/launchStream.sh
@@ -2,14 +2,3 @@
 mjpg_streamer -i "input_uvc.so -y -f 20 -r 640x480" -o "output_http.so -w /home/pi/mjpg-streamer/mjpg-streamer/www" -b
 v4l2-ctl -c exposure_auto=1
 v4l2-ctl -c exposure_absolute=0
-source /usr/local/bin/virtualenvwrapper.sh
-workon cv
-
-while true
-do
-	python ~/Nathan\ -\ tracking.py
-done
-
-deactivate
-killall mjpg_streamer
-#sudo shutdown -h now
