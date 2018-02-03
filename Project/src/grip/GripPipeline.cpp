d4d38fdd66ca6a46bc3a577124fe2c6135ecf6c1
kirac2093 <kirac2093@gmail.com>
2018-02-03 13:32:48 -0800

    Changed pipeline

--- Project/src/grip/GripPipeline.cpp
+++ Project/src/grip/GripPipeline.cpp
@@ -1,73 +1,77 @@
 #include "GripPipeline.h"
+/**
+* Initializes a GripPipeline.
+*/
 
 namespace grip {
 
 GripPipeline::GripPipeline() {
 }
 /**
-* Runs an iteration of the pipeline and updates outputs.
+* Runs an iteration of the Pipeline and updates outputs.
+*
+* Sources need to be set before calling this method. 
+*
 */
-void GripPipeline::Process(cv::Mat& source0){
+void GripPipeline::process(cv::Mat source0){
 	//Step HSV_Threshold0:
 	//input
 	cv::Mat hsvThresholdInput = source0;
-	double hsvThresholdHue[] = {0.0, 180.0};
-	double hsvThresholdSaturation[] = {0.0, 41.77474402730375};
-	double hsvThresholdValue[] = {224.73021582733813, 255.0};
+	double hsvThresholdHue[] = {59.89208633093526, 102.72727272727273};
+	double hsvThresholdSaturation[] = {121.48331037672557, 238.36062670548077};
+	double hsvThresholdValue[] = {199.50539568345323, 255.0};
 	hsvThreshold(hsvThresholdInput, hsvThresholdHue, hsvThresholdSaturation, hsvThresholdValue, this->hsvThresholdOutput);
+	//Step Blur0:
+	//input
+	cv::Mat blurInput = hsvThresholdOutput;
+	BlurType blurType = BlurType::MEDIAN;
+	double blurRadius = 9.009009009009006;  // default Double
+	blur(blurInput, blurType, blurRadius, this->blurOutput);
 	//Step Find_Contours0:
 	//input
-	cv::Mat findContoursInput = hsvThresholdOutput;
+	cv::Mat findContoursInput = blurOutput;
 	bool findContoursExternalOnly = false;  // default Boolean
 	findContours(findContoursInput, findContoursExternalOnly, this->findContoursOutput);
 	//Step Convex_Hulls0:
 	//input
 	std::vector<std::vector<cv::Point> > convexHullsContours = findContoursOutput;
 	convexHulls(convexHullsContours, this->convexHullsOutput);
-	//Step Filter_Contours0:
-	//input
-	std::vector<std::vector<cv::Point> > filterContoursContours = convexHullsOutput;
-	double filterContoursMinArea = 0.0;  // default Double
-	double filterContoursMinPerimeter = 0.0;  // default Double
-	double filterContoursMinWidth = 0.0;  // default Double
-	double filterContoursMaxWidth = 1000.0;  // default Double
-	double filterContoursMinHeight = 0.0;  // default Double
-	double filterContoursMaxHeight = 50.0;  // default Double
-	double filterContoursSolidity[] = {0.0, 100.0};
-	double filterContoursMaxVertices = 1000000.0;  // default Double
-	double filterContoursMinVertices = 0.0;  // default Double
-	double filterContoursMinRatio = 20.0;  // default Double
-	double filterContoursMaxRatio = 1000.0;  // default Double
-	filterContours(filterContoursContours, filterContoursMinArea, filterContoursMinPerimeter, filterContoursMinWidth, filterContoursMaxWidth, filterContoursMinHeight, filterContoursMaxHeight, filterContoursSolidity, filterContoursMaxVertices, filterContoursMinVertices, filterContoursMinRatio, filterContoursMaxRatio, this->filterContoursOutput);
 }
 
+/**
+ * This method is a generated setter for source0.
+ * @param source the Mat to set
+ */
+void GripPipeline::setsource0(cv::Mat &source0){
+	source0.copyTo(this->source0);
+}
 /**
  * This method is a generated getter for the output of a HSV_Threshold.
  * @return Mat output from HSV_Threshold.
  */
-cv::Mat* GripPipeline::GetHsvThresholdOutput(){
+cv::Mat* GripPipeline::gethsvThresholdOutput(){
 	return &(this->hsvThresholdOutput);
+}
+/**
+ * This method is a generated getter for the output of a Blur.
+ * @return Mat output from Blur.
+ */
+cv::Mat* GripPipeline::getblurOutput(){
+	return &(this->blurOutput);
 }
 /**
  * This method is a generated getter for the output of a Find_Contours.
  * @return ContoursReport output from Find_Contours.
  */
-std::vector<std::vector<cv::Point> >* GripPipeline::GetFindContoursOutput(){
+std::vector<std::vector<cv::Point> >* GripPipeline::getfindContoursOutput(){
 	return &(this->findContoursOutput);
 }
 /**
  * This method is a generated getter for the output of a Convex_Hulls.
  * @return ContoursReport output from Convex_Hulls.
  */
-std::vector<std::vector<cv::Point> >* GripPipeline::GetConvexHullsOutput(){
+std::vector<std::vector<cv::Point> >* GripPipeline::getconvexHullsOutput(){
 	return &(this->convexHullsOutput);
-}
-/**
- * This method is a generated getter for the output of a Filter_Contours.
- * @return ContoursReport output from Filter_Contours.
- */
-std::vector<std::vector<cv::Point> >* GripPipeline::GetFilterContoursOutput(){
-	return &(this->filterContoursOutput);
 }
 	/**
 	 * Segment an image based on hue, saturation, and value ranges.
@@ -83,6 +87,35 @@ std::vector<std::vector<cv::Point> >* GripPipeline::GetFilterContoursOutput(){
 		cv::inRange(out,cv::Scalar(hue[0], sat[0], val[0]), cv::Scalar(hue[1], sat[1], val[1]), out);
 	}
 
+	/**
+	 * Softens an image using one of several filters.
+	 *
+	 * @param input The image on which to perform the blur.
+	 * @param type The blurType to perform.
+	 * @param doubleRadius The radius for the blur.
+	 * @param output The image in which to store the output.
+	 */
+	void GripPipeline::blur(cv::Mat &input, BlurType &type, double doubleRadius, cv::Mat &output) {
+		int radius = (int)(doubleRadius + 0.5);
+		int kernelSize;
+		switch(type) {
+			case BOX:
+				kernelSize = 2 * radius + 1;
+				cv::blur(input,output,cv::Size(kernelSize, kernelSize));
+				break;
+			case GAUSSIAN:
+				kernelSize = 6 * radius + 1;
+				cv::GaussianBlur(input, output, cv::Size(kernelSize, kernelSize), radius);
+				break;
+			case MEDIAN:
+				kernelSize = 2 * radius + 1;
+				cv::medianBlur(input, output, kernelSize);
+				break;
+			case BILATERAL:
+				cv::bilateralFilter(input, output, -1, radius, radius);
+				break;
+        }
+	}
 	/**
 	 * Finds contours in an image.
 	 *
@@ -111,43 +144,6 @@ std::vector<std::vector<cv::Point> >* GripPipeline::GetFilterContoursOutput(){
 			cv::convexHull(cv::Mat((inputContours)[i]), hull[i], false);
 		}
 		outputContours = hull;
-	}
-
-
-	/**
-	 * Filters through contours.
-	 * @param inputContours is the input vector of contours.
-	 * @param minArea is the minimum area of a contour that will be kept.
-	 * @param minPerimeter is the minimum perimeter of a contour that will be kept.
-	 * @param minWidth minimum width of a contour.
-	 * @param maxWidth maximum width.
-	 * @param minHeight minimum height.
-	 * @param maxHeight  maximimum height.
-	 * @param solidity the minimum and maximum solidity of a contour.
-	 * @param minVertexCount minimum vertex Count of the contours.
-	 * @param maxVertexCount maximum vertex Count.
-	 * @param minRatio minimum ratio of width to height.
-	 * @param maxRatio maximum ratio of width to height.
-	 * @param output vector of filtered contours.
-	 */
-	void GripPipeline::filterContours(std::vector<std::vector<cv::Point> > &inputContours, double minArea, double minPerimeter, double minWidth, double maxWidth, double minHeight, double maxHeight, double solidity[], double maxVertexCount, double minVertexCount, double minRatio, double maxRatio, std::vector<std::vector<cv::Point> > &output) {
-		std::vector<cv::Point> hull;
-		output.clear();
-		for (std::vector<cv::Point> contour: inputContours) {
-			cv::Rect bb = boundingRect(contour);
-			if (bb.width < minWidth || bb.width > maxWidth) continue;
-			if (bb.height < minHeight || bb.height > maxHeight) continue;
-			double area = cv::contourArea(contour);
-			if (area < minArea) continue;
-			if (arcLength(contour, true) < minPerimeter) continue;
-			cv::convexHull(cv::Mat(contour, true), hull);
-			double solid = 100 * area / cv::contourArea(hull);
-			if (solid < solidity[0] || solid > solidity[1]) continue;
-			if (contour.size() < minVertexCount || contour.size() > maxVertexCount)	continue;
-			double ratio = (double) bb.width / (double) bb.height;
-			if (ratio < minRatio || ratio > maxRatio) continue;
-			output.push_back(contour);
-		}
 	}
 
 
