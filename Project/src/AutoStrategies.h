27b821223a0d7a86be4c3c220cb9cf3d902a9052
Brayton Kerekffy <mesadog14@gmail.com>
2018-02-17 10:59:08 -0800

    Put correct rotation angles

--- Project/src/AutoStrategies.h
+++ Project/src/AutoStrategies.h
@@ -37,24 +37,24 @@ namespace AUTO_STRATEGIES
 		{
 			// Drive from wall to distance of three feet from wall. Drives along hypotenuse of length two feet at angle of 45 degrees.
 			{AutoController::DRIVE_TO, 36, 0.2f},
-			{AutoController::ROTATE_TO, -30, 0.2f},
+			{AutoController::ROTATE_TO, 30, 0.2f},
 
 			{AutoController::RESET_DIST_0, 0, 0},
 			{AutoController::DRIVE_TO, 68.4, 0.2f},
-			{AutoController::ROTATE_TO, -60, 0.2f},
+			{AutoController::ROTATE_TO, 60, 0.2f},
 
 			{AutoController::RESET_DIST_0, 0, 0},
 			{AutoController::DRIVE_TO, 68.4, 0.2f},
-			{AutoController::ROTATE_TO, -90, 0.2f},
+			{AutoController::ROTATE_TO, 90, 0.2f},
 
 			// Drive left down field 12.011 feet.
 			{AutoController::RESET_DIST_0, 0, 0},
 			{AutoController::DRIVE_TO, 131.868 - 33.84, 0.2f},
-			{AutoController::ROTATE_TO, -135, 0.2f},
+			{AutoController::ROTATE_TO, 135, 0.2f},
 
 			{AutoController::RESET_DIST_0, 0, 0},
 			{AutoController::DRIVE_TO, 24, 0.2f},
-			{AutoController::ROTATE_TO, -180, 0.2f},
+			{AutoController::ROTATE_TO, 180, 0.2f},
 
 			// Drive towards wall for 30 inches (slightly less than three feet).
 			{AutoController::RESET_DIST_0, 0, 0},
@@ -67,11 +67,20 @@ namespace AUTO_STRATEGIES
 
 	const AutoController::Instruction AUTOLINE_STEPS[] =
 	{
-
 			{AutoController::DRIVE_TO, 144, .2f}
 	};
 	const AutoController::InstructionSet AUTOLINE = {AUTOLINE_STEPS, 1};
 
+	/**const AutoController::Instruction LOOKDANGCOOL_STEPS[] =
+	{
+			{AutoController::DRIVE_TO, 144, .2f},
+			{AutoController::ROTATE_TO, -90, .2f},
+
+			{AutoController::RESET_DIST_0, 0, 0},
+			{AutoController::DRIVE_TO, 144, .2f},
+
+
+	};**/
 }
 
 #endif
