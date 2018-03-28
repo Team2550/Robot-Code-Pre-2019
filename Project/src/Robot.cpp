28bdd6ad638f5a93f80a8388c410db5137f31646
Team 2550 Laptop <team2550robotics@gmail.com>
2018-03-28 10:32:24 -0700

    Fixed build error

--- Project/src/Robot.cpp
+++ Project/src/Robot.cpp
@@ -4,7 +4,7 @@
 // driveBase:  (float) max power, (float) max boost power, (int) left motor port,
 //             (int) right motor port
 Robot::Robot() : driveController(0), perifController(1),
-				 autoController(&driveBase, &bulldozer, &gyroscope),
+				 autoController(&driveBase, &bulldozer, &clamp, &gyroscope),
                  ultrasonic(0, (5 / 4.88) * (1000 / 25.4), 1), // (5 mm / 4.88 mV) * (1/25.4 in/mm) * (1000 mV/V)
 				 gyroscope(frc::SPI::Port::kOnboardCS0),
 				 driveBase(0, 1, 0, 1, 2, 3, 6.07 * M_PI, 512), // Pulses per rotation is set by encoder DIP switch. 512 PPR uses DIP switch configuration 0001.
